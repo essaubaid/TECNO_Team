b9e1c8deccd629d09f409aa1b151b812acf30935
Essa Sheikh <essaubaid0@gmail.com>
2021-12-07 01:21:07 +0500

    Essa Commit Style Sheet Updated -- product Details Control updated Home Page_incomplete Tile updated login signup and set up shop done with navigation

--- src/main/java/com/example/tecno/ProductPageController.java
+++ src/main/java/com/example/tecno/ProductPageController.java
@@ -1,19 +1,76 @@
 package com.example.tecno;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import model.productDetails;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class ProductPageController {
+public class ProductPageController implements Initializable {
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private Label phoneNoLabel;
+
+    @FXML
+    private Label priceLabel;
+
+    @FXML
+    private Text productNameLabel;
+
+    @FXML
+    private Label ramLabel;
+
+    @FXML
+    private Label sellerNameLabel;
+
+    @FXML
+    private Label shopNameLabel;
+
+    @FXML
+    private Label shopNoLabel;
+
+    @FXML
+    private Label storageLabel;
+
+    @FXML
+    private Label warrantyLabel;
 
     private Stage stage;
     private Scene scene;
     private Parent root;
+
+    private productDetails productdetails;
+
+    public void setData(){
+        productNameLabel.setText("Google Pixel 2 XL");
+
+        priceLabel.setText("Rs "+ 90000);
+        ramLabel.setText("8GB");
+        storageLabel.setText("256GB");
+        warrantyLabel.setText("1 Year");
+
+        sellerNameLabel.setText("Pawan Kumar");
+        shopNameLabel.setText("Pawan Electronics");
+        phoneNoLabel.setText("0300-0000000");
+        shopNoLabel.setText("23");
+
+        descriptionLabel.setText("Example");
+    }
+
+
 
     public void switchToSignIN(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("loginPage.fxml"));
@@ -61,5 +118,10 @@ public class ProductPageController {
         stage.setScene(scene);
         stage.show();
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setData();
     }
 }
