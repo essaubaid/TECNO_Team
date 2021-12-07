a158d35c22d4913d67e4f47a8423603246762f06
Essa Sheikh <essaubaid0@gmail.com>
2021-12-07 17:26:02 +0500

    Essa Commit HomePage profile updated

--- src/main/java/com/example/tecno/HomePageController.java
+++ src/main/java/com/example/tecno/HomePageController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
@@ -28,15 +29,19 @@ public class HomePageController implements Initializable {
     private ScrollPane scroll;
 
     @FXML
+    private Label Title;
+
+    @FXML
     private MenuButton dropDown;
 
     @FXML
     private Button favoritesButton;
 
+    @FXML
+    private Button letSeeButton;
+
     private List<ProductTileView> tiles = new ArrayList<>();
 
-    int column = 0;
-    int row = 1;
 
     private List getData(){
         List<ProductTileView> tiles = new ArrayList<>();
@@ -63,7 +68,32 @@ public class HomePageController implements Initializable {
         return tiles;
     }
 
+    public void switchToProfile(ActionEvent event) throws IOException {
+        Title.setText("My Profile");
+        grid.getChildren().removeAll(grid.getChildren());
+
+        FXMLLoader fxmlLoader = new FXMLLoader();
+        fxmlLoader.setLocation(getClass().getResource("view-profile.fxml"));
+        AnchorPane anchorPane = fxmlLoader.load();
+
+        grid.add(anchorPane, 1, 1); //(child,column,row)
+        //set grid width
+        grid.setMinWidth(Region.USE_COMPUTED_SIZE);
+        grid.setPrefWidth(Region.USE_COMPUTED_SIZE);
+        grid.setMaxWidth(Region.USE_PREF_SIZE);
+
+        //set grid height
+        grid.setMinHeight(Region.USE_COMPUTED_SIZE);
+        grid.setPrefHeight(Region.USE_COMPUTED_SIZE);
+        grid.setMaxHeight(Region.USE_PREF_SIZE);
+
+        GridPane.setMargin(anchorPane, new Insets(10, 5, 10, 70));
+    }
+
     public void switchToFavorites(ActionEvent event) throws IOException {
+        Title.setText("My Favorites");
+        int column = 0;
+        int row = 1;
 
         grid.getChildren().removeAll(grid.getChildren());
         for(int i = 0; i < tiles.size(); i++){
@@ -106,8 +136,8 @@ public class HomePageController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         tiles.addAll(getData());
-        column = 0;
-        row = 1;
+        int column = 0;
+        int row = 1;
 
         for(int i = 0; i < tiles.size(); i++){
             try {
