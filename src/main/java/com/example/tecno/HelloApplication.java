c331b65710e6fe6d5af34fada1b12759a3c2944f
Essa Sheikh <essaubaid0@gmail.com>
2021-12-08 18:00:55 +0500

    Essa Commit Admin Complete

--- src/main/java/com/example/tecno/HelloApplication.java
+++ src/main/java/com/example/tecno/HelloApplication.java
@@ -18,8 +18,8 @@ public class HelloApplication extends Application {
     public void start(@NotNull Stage stage) throws IOException, SQLException, ClassNotFoundException {
         Class.forName("com.mysql.jdbc.Driver");
         conn=DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/techno","root","DB@@2021");
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("adminHomePage.fxml"));
+                "jdbc:mysql://localhost:3310/techno","root","1234");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("loginPage.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("TECNO");
         stage.setScene(scene);
--- src/main/java/com/example/tecno/loginPageController.java
+++ src/main/java/com/example/tecno/loginPageController.java
@@ -10,8 +10,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import model.ProductTileView;
 
 import java.io.IOException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import static com.example.tecno.HelloApplication.conn;
 
 public class loginPageController {
 
@@ -51,6 +57,37 @@ public class loginPageController {
 
     }
 
+    public void initiateLogin(ActionEvent event) throws SQLException, IOException {
+        String username = usernameTextBox.getText();
+        String password = passwordTextBox.getText();
+        String Return = "";
+
+        String query = "select verifyLogin(?, ?);"; // query to check if id exists
+
+        PreparedStatement st = conn.prepareStatement(query); //creating and preparing statements
+
+        st.setString(1, "essa");
+        st.setString(2, "1234");
+        ResultSet rs = st.executeQuery();
+
+
+        while(rs.next()){
+            Return = rs.getString(1);
+        }
+
+        switch (Return){
+            case "admin":
+                switchToAdminPage(event);
+                break;
+            case "user":
+                switchToHomePage(event);
+                break;
+            case "shopkeeper":
+                break;
+        }
+
+    }
+
     public void switchToSetShop(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("setUpShop.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -61,6 +98,14 @@ public class loginPageController {
     }
     public void switchToHomePage(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+    public void switchToAdminPage(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("adminHomePage.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
